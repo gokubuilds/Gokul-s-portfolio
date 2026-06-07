59bf70c6d312569e8072656048bbf80ae88f3396
Gokul Soundarapandian <gokulsoundarapandian22@gmail.com>
2026-06-07 19:50:07 +0530

    removed ollama

--- chatbot_backend/rag.py
+++ chatbot_backend/rag.py
@@ -1,7 +1,6 @@
 import os
 from langchain_groq import ChatGroq
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_ollama import OllamaEmbeddings
 from langchain_community.document_loaders import DirectoryLoader, TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
